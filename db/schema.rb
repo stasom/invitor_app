ec2469f6c7c09dfcc13198160c6cea8538a0073f
Soma Stagel <soma@appmaintainers.com>
2014-10-13 14:10:12 +0200

    migration error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012073121) do
+ActiveRecord::Schema.define(version: 20141013120519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
